c24d969e55e6c1866ad15db6b9f9c75c20caffa4
John <jhezell@go.olemiss.edu>
2020-12-06 23:59:20 -0800

    Added print function and changed default name

--- pages/generate.js
+++ pages/generate.js
@@ -5,7 +5,7 @@ import ImgBox from "../components/ImgBox"
 export default function Generate() {
 
     const [image, setImage] = useState([])
-    const [text, setText] = useState("John")
+    const [text, setText] = useState("...")
 
 
     useEffect(() => {
@@ -24,23 +24,31 @@ export default function Generate() {
 
     return(
         <div>
-            <div className="h-screen flex flex-col justify-center items-center bg-pink-300">
+            <div id="printable" className="h-screen flex flex-col justify-center bg-pink-300 relative">
 
-                {image.map(image => (
-                    <ImgBox url={image.urls.full} key={image.id} name={text}/>
-                ))}
-                <form className="text-2xl py-12 px-2">
-                    <input
-                        className="rounded border-4 border-white text-center bg-pink-300 text-white placeholder-white"
-                        type="text"
-                        placeholder="Your Name"
-                        name="cardText"
-                        onChange={handleChange}
-                    />
-                </form>
+                <div className="absolute w-full top-0 flex justify-end p-6">
+                    <button
+                        className="noel-button"
+                        onClick={() => window.print()}>
+                        Print</button>
+                </div>
+
+                <div className="flex flex-col justify-center items-center">
+                    {image.map(image => (
+                        <ImgBox url={image.urls.full} key={image.id} name={text}/>
+                    ))}
+                    <form className="text-2xl py-12 px-2">
+                        <input
+                            className="rounded-lg border-4 border-white text-center bg-pink-300 text-white placeholder-white"
+                            type="text"
+                            placeholder="Your Name"
+                            name="cardText"
+                            onChange={handleChange}
+                        />
+                    </form>
+                </div>
+
             </div>
-
-
         </div>
 
     )
